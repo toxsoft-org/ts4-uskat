373bdc7f3a252d5b1471b86ba60e262b708f4438
mvk <kovach.mike@gmail.com>
2022-07-24 12:53:33 +0700

    uskat.s5.server. histdata. Промежуточный 2

--- org.toxsoft.uskat.core/src/org/toxsoft/uskat/core/backend/api/BaMsgRtdataHistData.java
+++ org.toxsoft.uskat.core/src/org/toxsoft/uskat/core/backend/api/BaMsgRtdataHistData.java
@@ -13,12 +13,15 @@ import org.toxsoft.core.tslib.bricks.strio.chario.impl.CharInputStreamString;
 import org.toxsoft.core.tslib.bricks.strio.chario.impl.CharOutputStreamAppendable;
 import org.toxsoft.core.tslib.bricks.strio.impl.StrioReader;
 import org.toxsoft.core.tslib.bricks.strio.impl.StrioWriter;
+import org.toxsoft.core.tslib.bricks.time.ITimeInterval;
 import org.toxsoft.core.tslib.bricks.time.ITimedList;
+import org.toxsoft.core.tslib.bricks.time.impl.TimeInterval;
 import org.toxsoft.core.tslib.bricks.time.impl.TimedList;
 import org.toxsoft.core.tslib.coll.IMap;
 import org.toxsoft.core.tslib.coll.IMapEdit;
 import org.toxsoft.core.tslib.coll.impl.ElemMap;
 import org.toxsoft.core.tslib.gw.gwid.Gwid;
+import org.toxsoft.core.tslib.utils.Pair;
 import org.toxsoft.core.tslib.utils.errors.TsNullArgumentRtException;
 import org.toxsoft.uskat.core.api.rtdserv.ISkRtdataService;
 
@@ -50,18 +53,25 @@ public class BaMsgRtdataHistData
   /**
    * Creates the message instance.
    *
-   * @param aNewValues {@link IMap}&lt;{@link Gwid},{@link ITimedList}&gt; - map "RTdata GWID" - "historical values"
+   * @param aNewValues {@link IMap}&lt;{@link Gwid},{@link Pair}&lt;{@link ITimeInterval},{@link ITimedList}&gt;&gt; -
+   *          map "RTdata GWID" - "historical values"
    * @return {@link GtMessage} - created instance of the message
    * @throws TsNullArgumentRtException any argument = <code>null</code>
    */
-  public GtMessage makeMessage( IMap<Gwid, ITimedList<ITemporalAtomicValue>> aNewValues ) {
+  public GtMessage makeMessage( IMap<Gwid, Pair<ITimeInterval, ITimedList<ITemporalAtomicValue>>> aNewValues ) {
     StringBuilder sb = new StringBuilder();
     IStrioWriter sw = new StrioWriter( new CharOutputStreamAppendable( sb ) );
     sw.writeChar( CHAR_ARRAY_BEGIN );
     for( int i = 0, count = aNewValues.size(); i < count; i++ ) {
       Gwid g = aNewValues.keys().get( i );
-      ITimedList<ITemporalAtomicValue> v = aNewValues.values().get( i );
+      Pair<ITimeInterval, ITimedList<ITemporalAtomicValue>> p = aNewValues.values().get( i );
+      ITimeInterval interval = p.left();
+      ITimedList<ITemporalAtomicValue> v = p.right();
       Gwid.KEEPER.write( sw, g );
+      sw.writeSeparatorChar();
+      sw.writeLong( interval.startTime() );
+      sw.writeSeparatorChar();
+      sw.writeLong( interval.endTime() );
       sw.writeSeparatorChar();
       TemporalAtomicValueKeeper.KEEPER.writeColl( sw, v, false );
       if( i < count - 1 ) {
@@ -76,18 +86,23 @@ public class BaMsgRtdataHistData
    * Extracts new current RTdata values argument from the message.
    *
    * @param aMsg {@link GenericMessage} - the message
-   * @return {@link IMap}&lt;{@link Gwid},{@link ITimedList}&gt; - map "RTdata GWID" - "historical value"
+   * @return {@link IMap}&lt;{@link Gwid},{@link Pair}&lt;{@link ITimeInterval},{@link ITimedList}&gt;&gt; - map "RTdata
+   *         GWID" - "historical value"
    */
-  public IMap<Gwid, ITimedList<ITemporalAtomicValue>> getNewValues( GenericMessage aMsg ) {
+  public IMap<Gwid, Pair<ITimeInterval, ITimedList<ITemporalAtomicValue>>> getNewValues( GenericMessage aMsg ) {
     String s = getArg( aMsg, ARGID_NEW_VALUES ).asString();
-    IMapEdit<Gwid, ITimedList<ITemporalAtomicValue>> map = new ElemMap<>();
+    IMapEdit<Gwid, Pair<ITimeInterval, ITimedList<ITemporalAtomicValue>>> map = new ElemMap<>();
     IStrioReader sr = new StrioReader( new CharInputStreamString( s ) );
     if( sr.readArrayBegin() ) {
       do {
         Gwid g = Gwid.KEEPER.read( sr );
         sr.ensureSeparatorChar();
+        long startTime = sr.readLong();
+        sr.ensureSeparatorChar();
+        long endTime = sr.readLong();
+        sr.ensureSeparatorChar();
         ITimedList<ITemporalAtomicValue> v = new TimedList<>( TemporalAtomicValueKeeper.KEEPER.readColl( sr ) );
-        map.put( g, v );
+        map.put( g, new Pair<>( new TimeInterval( startTime, endTime ), v ) );
       } while( sr.readArrayNext() );
     }
     return map;
