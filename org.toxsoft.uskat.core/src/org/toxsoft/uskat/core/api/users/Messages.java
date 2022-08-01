89d9b5d3c9fcd65c2e100184fe3b2963ccdc3fc1
goga <goga@GOGAW-PC-LIIN>
2022-08-01 13:42:31 +0300

    Minor bug fixes

--- org.toxsoft.uskat.core/src/org/toxsoft/uskat/core/api/users/Messages.java
+++ org.toxsoft.uskat.core/src/org/toxsoft/uskat/core/api/users/Messages.java
@@ -2,21 +2,23 @@ package org.toxsoft.uskat.core.api.users;
 
 import java.util.*;
 
+@SuppressWarnings( "javadoc" )
 public class Messages {
 
-  private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".messages"; //$NON-NLS-1$
+  private static final String BUNDLE_NAME = Messages.class.getName().toLowerCase();
 
   private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle( BUNDLE_NAME );
 
   private Messages() {
   }
 
-  public static String getString( String key ) {
+  public static String getString( String aKey ) {
     try {
-      return RESOURCE_BUNDLE.getString( key );
+      return RESOURCE_BUNDLE.getString( aKey );
     }
-    catch( MissingResourceException e ) {
-      return '!' + key + '!';
+    catch( @SuppressWarnings( "unused" ) MissingResourceException e ) {
+      return '!' + aKey + '!';
     }
   }
+
 }
