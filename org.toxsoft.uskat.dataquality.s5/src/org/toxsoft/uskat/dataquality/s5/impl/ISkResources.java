3ca065cfe81d2abe503b1d243b1fdbed4076ff90
mvk <kovach.mike@gmail.com>
2022-11-09 18:42:48 +0700

    +org.toxsoft.uskat.dataquality.s5. maven-сборка dataquality

---
+++ org.toxsoft.uskat.dataquality.s5/src/org/toxsoft/uskat/dataquality/s5/impl/ISkResources.java
@@ -0,0 +1,11 @@
+package org.toxsoft.uskat.dataquality.s5.impl;
+
+/**
+ * Localizable resources.
+ *
+ * @author hazard157
+ */
+// @SuppressWarnings( "nls" )
+interface ISkResources {
+  // TODO:
+}
