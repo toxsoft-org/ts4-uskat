65c4b80595d457c84c3d60f3cdfde3a4a151c18d
goga <goga@GOGAW-PC-LIIN>
2022-12-15 11:30:37 +0300

    Woking on IClobDb

--- org.toxsoft.uskat.core/src/org/toxsoft/uskat/core/devapi/clobdb/IClobDbSection.java
+++ org.toxsoft.uskat.core/src/org/toxsoft/uskat/core/devapi/clobdb/IClobDbSection.java
@@ -2,14 +2,18 @@ package org.toxsoft.uskat.core.devapi.clobdb;
 
 import org.toxsoft.core.tslib.bricks.strid.more.*;
 import org.toxsoft.core.tslib.coll.*;
+import org.toxsoft.core.tslib.coll.basis.*;
 
-public interface IClobDbSection {
+public interface IClobDbSection
+    extends ITsClearable {
 
   /**
-   * Lists IDs of existing CLOBs under the specified
+   * Lists IDs of existing CLOBs under the specified {@link IdChain}.
+   * <p>
+   * If argument is {@link IdChain#NULL} than returns all keys in the section.
    *
-   * @param aPrefix
-   * @return
+   * @param aPrefix {@link IdChain} - the node ID
+   * @return {@link IList}&lt; {@link IdChain} &gt; - the IDs list
    */
   IList<IdChain> listChildIds( IdChain aPrefix );
 
