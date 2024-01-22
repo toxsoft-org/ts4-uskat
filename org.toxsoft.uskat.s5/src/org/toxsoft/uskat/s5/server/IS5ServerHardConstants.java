e59f78c19c6497f53d9e9a1e0eceb5fb9a331786
mvk <kovach.mike@gmail.com>
2024-01-22 14:26:30 +0700

    check maven build (s5 version = 4.22)

--- org.toxsoft.uskat.s5/src/org/toxsoft/uskat/s5/server/IS5ServerHardConstants.java
+++ org.toxsoft.uskat.s5/src/org/toxsoft/uskat/s5/server/IS5ServerHardConstants.java
@@ -60,7 +60,7 @@ public interface IS5ServerHardConstants
   /**
    * Версия сервера.
    */
-  TsVersion version = new TsVersion( 4, 21, 2023, Month.DECEMBER, 2 );
+  TsVersion version = new TsVersion( 4, 22, 2024, Month.JANUARY, 22 );
 
   // ------------------------------------------------------------------------------------
   // Опции s5-backend
