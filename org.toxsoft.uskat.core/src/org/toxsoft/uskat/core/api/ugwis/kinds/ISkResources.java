0fd12c9226b39b818517105850e47a77af76161b
Dmitry Egorov <egorov.dmitry.alex@gmail.com>
2024-06-04 16:57:15 +0300

    add UgwiKindSkRtdata

--- org.toxsoft.uskat.core/src/org/toxsoft/uskat/core/api/ugwis/kinds/ISkResources.java
+++ org.toxsoft.uskat.core/src/org/toxsoft/uskat/core/api/ugwis/kinds/ISkResources.java
@@ -18,4 +18,8 @@ interface ISkResources {
 
   String STR_UK_LINK   = Messages.getString( "STR_UK_LINK" );   //$NON-NLS-1$
   String STR_UK_LINK_D = Messages.getString( "STR_UK_LINK_D" ); //$NON-NLS-1$
+
+  String STR_UK_RTDATA   = "Rtdata value";
+  String STR_UK_RTDATA_D = "The Sk-object Rtdata vlaue";
+
 }
