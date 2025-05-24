dc4665f89cab234736e0cd0aec26fe61c019503c
tsdev <tsdev@baiterek-arm>
2025-05-24 21:07:38 +0300

    uskat.backend.s5.gui: +S5BackendGuiUtils

--- org.toxsoft.uskat.backend.s5.gui/src/org/toxsoft/uskat/backend/s5/gui/utils/ISkResources.java
+++ org.toxsoft.uskat.backend.s5.gui/src/org/toxsoft/uskat/backend/s5/gui/utils/ISkResources.java
@@ -5,9 +5,11 @@ package org.toxsoft.uskat.backend.s5.gui.utils;
  *
  * @author hazard157
  */
+@SuppressWarnings( "nls" )
 interface ISkResources {
 
-  String STR_S5_CONN_CFG_PROVIDER   = Messages.getString( "STR_S5_CONN_CFG_PROVIDER" );   //$NON-NLS-1$
-  String STR_S5_CONN_CFG_PROVIDER_D = Messages.getString( "STR_S5_CONN_CFG_PROVIDER_D" ); //$NON-NLS-1$
+  String STR_S5_CONN_CFG_PROVIDER   = Messages.getString( "STR_S5_CONN_CFG_PROVIDER" );
+  String STR_S5_CONN_CFG_PROVIDER_D = Messages.getString( "STR_S5_CONN_CFG_PROVIDER_D" );
 
+  String ERR_NO_CONNECT = "open s5-connection error: %s";
 }
---
+++ org.toxsoft.uskat.backend.s5.gui/src/org/toxsoft/uskat/backend/s5/gui/utils/S5BackendGuiUtils.java
@@ -0,0 +1,105 @@
+package org.toxsoft.uskat.backend.s5.gui.utils;
+
+import static org.toxsoft.uskat.backend.s5.gui.utils.ISkResources.*;
+
+import java.lang.reflect.*;
+
+import org.eclipse.swt.widgets.*;
+import org.toxsoft.core.tslib.bricks.ctx.*;
+import org.toxsoft.core.tslib.bricks.threadexec.*;
+import org.toxsoft.core.tslib.utils.errors.*;
+import org.toxsoft.core.tslib.utils.logs.impl.*;
+import org.toxsoft.uskat.core.connection.*;
+import org.toxsoft.uskat.core.gui.glib.query.*;
+import org.toxsoft.uskat.core.impl.*;
+import org.toxsoft.uskat.s5.client.remote.connection.*;
+
+/**
+ * Вспомогательные методы для работы с s5-бекендом в GUI
+ *
+ * @author mvk
+ */
+public class S5BackendGuiUtils {
+
+  private static final long ERR_CONNECTION_TIMEOUT = 500;
+
+  /**
+   * @param aShell {@link Shell} родительское окно
+   * @param aConnection {@link ISkConnection} открываемое соединение
+   * @param aConnectionContext {@link ITsContext} контекст открываемого соединения, параметр
+   *          {@link ISkConnection#open(ITsContextRo)}
+   * @param aText String текст в диалоге
+   * @param aTimeout long время (мсек) ожидания подключения. < 0: бесконечно.
+   * @throws TsNullArgumentRtException любой аргумент = null
+   */
+  @SuppressWarnings( "unused" )
+  public static void showProgressConnectionDialog( Shell aShell, ISkConnection aConnection,
+      ITsContextRo aConnectionContext, String aText, long aTimeout ) {
+    TsNullArgumentRtException.checkNulls( aShell, aConnection, aConnectionContext, aText );
+
+    ITsThreadExecutor threadExecutor = ISkCoreConfigConstants.REFDEF_THREAD_EXECUTOR.getRef( aConnectionContext );
+
+    // Создание диалога прогресса выполнения запроса
+    ConnectionProgressDialog progressDialog = new ConnectionProgressDialog( aShell, aText, aTimeout );
+    progressDialog.setCancelHandler( () -> {
+      // Пользователь отменил операцию
+      progressDialog.cancel();
+    } );
+    try {
+      // fork = false, cancelable = true
+      progressDialog.run( true, true, aMonitor -> {
+        while( aConnection.state() != ESkConnState.ACTIVE ) {
+          if( progressDialog.isNeedCancel() ) {
+            // Пользователь отменил операцию
+            return;
+          }
+          threadExecutor.syncExec( () -> {
+            try {
+              aConnection.open( aConnectionContext );
+            }
+            catch( S5ConnectionException e ) {
+              LoggerUtils.errorLogger().error( ERR_NO_CONNECT, e.getLocalizedMessage() );
+            }
+            try {
+              Thread.sleep( ERR_CONNECTION_TIMEOUT );
+            }
+            catch( InterruptedException e ) {
+              // nop
+            }
+            catch( Exception e ) {
+              LoggerUtils.errorLogger().error( e );
+            }
+          } );
+        }
+        aMonitor.done();
+        LoggerUtils.defaultLogger().info( "Connection opened" ); //$NON-NLS-1$
+        return;
+      } );
+
+    }
+    catch( InvocationTargetException | InterruptedException ex ) {
+      LoggerUtils.errorLogger().error( ex );
+    }
+  }
+
+  /**
+   * Диалог подключения
+   */
+  private final static class ConnectionProgressDialog
+      extends SkProgressDialog {
+
+    boolean needCancel = false;
+
+    ConnectionProgressDialog( Shell aShell, String aDialogName, long aTimeout ) {
+      super( aShell, aDialogName, aTimeout );
+    }
+
+    boolean isNeedCancel() {
+      return needCancel;
+    }
+
+    void cancel() {
+      needCancel = true;
+    }
+  }
+}
