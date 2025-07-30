ae5aac0f6c565a0de28295fbbbc38dfd8dc5128e
tsdev <tsdev@nm-dch>
2025-07-30 18:37:43 +0700

    SkCoreServTransaction,commit: refresh changed list after event

--- org.toxsoft.uskat.core/src/org/toxsoft/uskat/core/impl/SkCoreServTransaction.java
+++ org.toxsoft.uskat.core/src/org/toxsoft/uskat/core/impl/SkCoreServTransaction.java
@@ -277,6 +277,14 @@ public class SkCoreServTransaction
 
     // fire before commit event
     eventer.fireBeforeCommitEvent( removingObjs, updatingObjs, creatingObjs, updatingLinks );
+
+    // refresh changed list after "before-commit-event" handle
+    // matters if the riveted objects are outside of a transaction
+    removingObjs = new ElemArrayList<>( objectManager.removingObjs().values() );
+    updatingObjs = new ElemArrayList<>( objectManager.updatingObjs().values() );
+    creatingObjs = new ElemArrayList<>( objectManager.creatingObjs().values() );
+    updatingLinks = new ElemArrayList<>( this.txUpdatingLinks );
+
     try {
       // TODO: добавить в метод аргумент обновляемые связи
       if( removingObjs.size() > 0 || updatingObjs.size() > 0 || creatingObjs.size() > 0 ) {
