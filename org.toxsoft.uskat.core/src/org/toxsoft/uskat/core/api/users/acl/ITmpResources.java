90ce74fad39e6d00a0ef89abf84d349ae63b5e18
denis <denis@denis-vm>
2026-03-13 16:13:28 +0400

    Localization

--- org.toxsoft.uskat.core/src/org/toxsoft/uskat/core/api/users/acl/ITmpResources.java
+++ org.toxsoft.uskat.core/src/org/toxsoft/uskat/core/api/users/acl/ITmpResources.java
@@ -1,12 +1,17 @@
 package org.toxsoft.uskat.core.api.users.acl;
 
+/**
+ * Localizable resources.
+ *
+ * @author hazard157
+ */
 interface ITmpResources {
 
-  String STR_SK_ACCESS_DENY    = "Deny";
-  String STR_SK_ACCESS_DENY_D  = "";
-  String STR_SK_ACCESS_READ    = "Read";
-  String STR_SK_ACCESS_READ_D  = "";
-  String STR_SK_ACCESS_WRITE   = "Write";
-  String STR_SK_ACCESS_WRITE_D = "";
+  String STR_SK_ACCESS_DENY    = Messages.getString( "STR_SK_ACCESS_DENY" );    //$NON-NLS-1$
+  String STR_SK_ACCESS_DENY_D  = Messages.getString( "STR_SK_ACCESS_DENY_D" );  //$NON-NLS-1$
+  String STR_SK_ACCESS_READ    = Messages.getString( "STR_SK_ACCESS_READ" );    //$NON-NLS-1$
+  String STR_SK_ACCESS_READ_D  = Messages.getString( "STR_SK_ACCESS_READ_D" );  //$NON-NLS-1$
+  String STR_SK_ACCESS_WRITE   = Messages.getString( "STR_SK_ACCESS_WRITE" );   //$NON-NLS-1$
+  String STR_SK_ACCESS_WRITE_D = Messages.getString( "STR_SK_ACCESS_WRITE_D" ); //$NON-NLS-1$
 
 }
