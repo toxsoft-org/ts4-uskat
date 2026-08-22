b3753b85074b067dfc6ea6711ee175248788000e
user <user@CI-STEND>
2026-08-23 07:03:28 +0800

    S5BackendQueriesAtomicValueFunctions. fix (remove endTime value) 2

--- org.toxsoft.uskat.s5/src/org/toxsoft/uskat/s5/server/backend/supports/queries/impl/S5BackendQueriesAtomicValueFunctions.java
+++ org.toxsoft.uskat.s5/src/org/toxsoft/uskat/s5/server/backend/supports/queries/impl/S5BackendQueriesAtomicValueFunctions.java
@@ -270,7 +270,7 @@ class S5BackendQueriesAtomicValueFunctions
     if( aValue == null ) {
       // У последовательности больше нет значений. Формирование последнего значения
       // 2026-08-22 mvk---
-      // addValue();
+      addValue();
       // Дополнение по необходимости пустыми значениями
       // addEmptyValues( interval.endTime() );
       // Передача сформированного результата
